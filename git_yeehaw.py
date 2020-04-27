5315ad3ac5fc00bbdb8a13efdbfee34b4ea35ccf
Filip Eriksson <f.eriksson.21.fe@gmail.com>
2020-04-27 15:58:26 +0200

    Now printing size and repo name of the repo

--- git_yeehaw.py
+++ git_yeehaw.py
@@ -1,12 +1,9 @@
 import urllib.request, json, random, os
 
-data = "no data :("
-
 base_url = "https://api.github.com/repositories?since="
 
 def get_json(url):
     response = urllib.request.urlopen(url)
-    global data
     return json.loads(response.read())
 
 def get_random_integer():
@@ -15,6 +12,13 @@ def get_random_integer():
 def get_random_project(json_data):
     return random.choice(json_data)
 
+def get_repo_size(repo_name):
+    url = 'https://api.github.com/repos/' + repo_name
+    response = urllib.request.urlopen(url)
+    project_json = json.loads(response.read())
+    size = int(project_json['size'])
+    return size
+
 if __name__ == "__main__":
     random_url = base_url + str(get_random_integer())
     json_data = get_json(random_url)
@@ -22,5 +26,12 @@ if __name__ == "__main__":
     project = get_random_project(json_data)
     
     print("Beep Boop")
-    os.system("git clone " + project['html_url'])
+    print("Oooh look at this repo: " + str(project['name']))
+    print('Size is: ' + str(get_repo_size(project['full_name'])) + "KB")
+    value = input("Do you want to continue? (yes, no): ")
+    if value.lower() == 'yes':
+        os.system("git clone " + project['html_url'])
+    print("Exiting git yeehaw...")
+
+
     
